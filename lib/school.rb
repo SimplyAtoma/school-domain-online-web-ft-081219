24b52ce8c82b75cab0a1d515777ae2fda4b44252
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-23 22:28:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+  attr_accessor roster
+  def initialize(school_name)
+    @school_name = school_name
+  end
+  ROSTER = {}
+  def add_student(student, grade)
+    unless ROSTER.include?(grade)
+      ROSTER[grade] = []
+    end
+    ROSTER[grade] << student
+  end 
+  
+  def grade
+    ROSTER[grade]
+  end 
+  
+  def sort 
+    
+  end
+end
